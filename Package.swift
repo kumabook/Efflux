d21099d37aa4dd3d605f31f33414a5acd5864202
Hiroki Kumamoto <kumabook@live.jp>
2019-04-15 18:36:17 +0900

    Remove dependency on ReactiveSwift

--- Package.swift
+++ Package.swift
@@ -11,15 +11,13 @@ let package = Package(
             name: "Efflux",
             targets: ["Efflux"]),
     ],
-    dependencies: [
-      .package(url: "https://github.com/ReactiveCocoa/ReactiveSwift.git", from: "5.0.0")
-    ],
+    dependencies: [],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Efflux",
-            dependencies: ["ReactiveSwift"]),
+            dependencies: []),
         .testTarget(
             name: "EffluxTests",
             dependencies: ["Efflux"]),
